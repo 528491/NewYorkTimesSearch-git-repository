51869c19b2f3c729723ed344f02726efd7dd3ee0
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-03-28 21:54:21 -0700

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,18 +1,17 @@
 
 $("#searchMe").on("click", function () {
 
-  var search_term = document.getElementById("searchTerm").value;
-  var numb_records = document.getElementById("numbRecords").value;
-  var start_year = document.getElementById("startYear").value;
-  var end_year = document.getElementById("endYear").value;
-
+  var search_term = $("#searchTerm").val();
+  var numb_records = $("#numbRecords").val();
+  var start_year = $("#startYear").val();
+  var end_year = $("#endYear").val();
 
   var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
   queryUrl += '?' + $.param({
     'api-key': "9652878909cb4e5584a00be6cc2abfa9",
-    'q': "obama",
-    'begin_date': "20080101",
-    'end_date': "20161231",
+    'q': search_term,
+    'begin_date': start_year+"0101",
+    'end_date': end_year+"1231",
     'sort': "newest",
     'fl': "web_url,keywords,headline,pub_date,source",
     'facet_field': "main"
